ba8435e3b46c47d8fe081912b7563334ac769c1d
MrBananaMan541 <112341486+MrBananaMan541@users.noreply.github.com>
2025-12-03 16:55:41 -0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -11,9 +11,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: ImagePickerDemo(),
-    );
+    return const MaterialApp(home: ImagePickerDemo());
   }
 }
 
@@ -25,6 +23,33 @@ class ImagePickerDemo extends StatefulWidget {
 }
 
 class _ImagePickerDemoState extends State<ImagePickerDemo> {
+  File? _image;
+  final List<File?> _images = [];
+  final ImagePicker _picker = ImagePicker();
+
+  Future<void> _takePhoto() async {
+    final XFile? photo = await _picker.pickImage(source: ImageSource.camera);
+
+    if (photo != null) {
+      setState(() {
+        _image = File(photo.path);
+        _images.add(_image);
+        //If I knew how to save the photo to phone gallery, then do it here,
+        //and then the user could pick an image from their gallery
+      });
+    }
+  }
+
+  Future<void> _pickFromGallery() async {
+    final XFile? photo = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (photo != null) {
+      setState(() {
+        _image = File(photo.path);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +57,18 @@ class _ImagePickerDemoState extends State<ImagePickerDemo> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('No image selected'),
+            if (_image != null)
+              Image.file(_image!, height: 300)
+            else
+              const Text('No image selected'),
+            ElevatedButton(
+              onPressed: _takePhoto,
+              child: const Text('Take Photo'),
+            ),
+            ElevatedButton(
+              onPressed: _pickFromGallery, 
+              child: const Text('Pick from Gallery')
+            )
           ],
         ),
       ),
